703e8b5f3a5168de35cfb1033de84c20b1f8fe30
STAKEME <79249177+stakemepro@users.noreply.github.com>
2022-11-13 16:43:40 +1000

    Update prepare-project.sh

--- prepare-project.sh
+++ prepare-project.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 apt-get install screen
 if [ ! -f "/root/.nvm/versions/node/v16.18.0/bin/node" ]; then
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash && source ~/.bash_profile && nvm install 16.18.0 && node -v
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+  touch $HOME/.bash_profile
+  echo 'export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> $HOME/.bash_profile
+  nvm install 16.18.0 && node -v
 fi

6210c72a28124a65ce01246b546b63ff92962d7f
STAKEME <79249177+stakemepro@users.noreply.github.com>
2022-11-13 16:45:12 +1000

    Update prepare-project.sh

--- prepare-project.sh
+++ prepare-project.sh
@@ -6,5 +6,6 @@ if [ ! -f "/root/.nvm/versions/node/v16.18.0/bin/node" ]; then
   echo 'export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> $HOME/.bash_profile
+  . $HOME/.bash_profile
   nvm install 16.18.0 && node -v
 fi

8690617c22af8483a3823874a5697c132f91bf8a
STAKEME <79249177+stakemepro@users.noreply.github.com>
2022-11-15 01:20:44 +1000

    Update nibiru-installer.sh

--- nodes/nibiru/nibiru-installer.sh
+++ nodes/nibiru/nibiru-installer.sh
@@ -60,9 +60,9 @@ WantedBy=multi-user.target
 EOF
 nibid tendermint unsafe-reset-all --home $HOME/.nibid --keep-addr-book
 
-SNAP_RPC="https://nibiru-testnet.nodejumper.io:443"
+SNAP_RPC="http://nibiru.stakeme.pro:36657"
 LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 1000)); \
 TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
 
 echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
@@ -72,3 +72,4 @@ s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
 s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
 s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/.nibid/config/config.toml
 
+sudo systemctl restart nibid

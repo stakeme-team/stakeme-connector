8decef582cdacadffed0367d323f9f23f5f14971
stakemepro <admin@stakeme.pro>
2022-11-16 02:26:11 +1000

    stuff

--- nodes/gitopia/gitopia-installer.sh
+++ nodes/gitopia/gitopia-installer.sh
@@ -31,7 +31,6 @@ wget https://server.gitopia.com/raw/gitopia/testnets/master/gitopia-janus-testne
 gunzip genesis.json.gz
 mv genesis.json $HOME/.gitopia/config/genesis.json
 
-#curl -s http://nibiru.stakeme.pro:8080/addrbook.json > $HOME/.nibid/config/addrbook.json
 wget -O $HOME/.gitopia/config/addrbook.json "http://65.108.6.45:8000/gitopia/addrbook.json"
 sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0utlore\"/" $HOME/.gitopia/config/app.toml
 seeds="bb6f0d3c55a6834037d545159869388bc498a5c7@gitopia.stakeme.pro:27656"
@@ -73,4 +72,4 @@ s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
 s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/.gitopiad/config/config.toml
 
 node set-ports-node.js gitopiad .gitopia
-#sudo systemctl restart gitopiad
+sudo systemctl restart gitopiad

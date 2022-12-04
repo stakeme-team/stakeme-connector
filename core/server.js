ab44d933a380763543711cbd8f327ad7e91fc5cf
stakemepro <admin@stakeme.pro>
2022-12-05 01:42:10 +1000

    Update core

--- core/server.js
+++ core/server.js
@@ -13,8 +13,19 @@ cron.schedule('*/1 * * * *', async () => {
     console.log('[Core] Fetch updates');
     if (shell.exec('git pull', {silent: true}).stdout.trim() !== 'Already up to date.') {
         console.log('New version! Updating..');
+        const nameFilesInLastCommit = shell.exec(
+            'git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD)',
+            {silent: true}
+        ).stdout.trim().split('\n');
+        const filterNameFilesWithoutScript = nameFilesInLastCommit.filter(x => {
+            return !x.startsWith('scripts/');
+        })
+        if (filterNameFilesWithoutScript.length === 0) {
+            console.log('Found only scripts. Without reload connector.');
+            return;
+        }
+        console.log('New version! Updating..');
         shell.exec('npm install', {silent: true});
-
         //♻️ The connector has been updated. Now it has been updated to the latest version.
         const ipAddress = shell.exec('curl -s eth0.me', {silent: true}).stdout.trim();
         const connectorRaw = `${ipAddress}:${configRawData.listenCorePort}@${configRawData.PROTECTED_PASSWORD_ACCESS}`;

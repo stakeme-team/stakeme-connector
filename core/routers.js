61a5cdac0630448137952ac55f468e69ebf922f6
stakemepro <admin@stakeme.pro>
2022-12-06 17:07:29 +1000

    stuff

--- core/routers.js
+++ core/routers.js
@@ -3,8 +3,8 @@ const api = require('./conrollers-api');
 const NodeManager = require('../nodes/NodeManager')
 
 const mainRouter = new express.Router();
-mainRouter.post('/cosmos', api.cosmosEndpoint(NodeManager));
-mainRouter.post('/cosmos/update', api.cosmosUpdate(NodeManager));
-mainRouter.post('/cosmos/installer', api.cosmosUpdate(NodeManager));
+mainRouter.post('/cosmos', api.cosmosEndpoint(NodeManager)());
+mainRouter.post('/cosmos/update', api.cosmosUpdate(NodeManager)());
+mainRouter.post('/cosmos/installer', api.cosmosUpdate(NodeManager)());
 
 exports.mainRouter = mainRouter;

ad4f0433e2560a95a7b77072546e7a4bdc19eb44
stakemepro <admin@stakeme.pro>
2022-12-06 17:52:06 +1000

    stuff

--- nodes/NodeManager.js
+++ nodes/NodeManager.js
@@ -6,25 +6,31 @@ const GitopiaNode = require("./gitopia/GitopiaNode");
 const configRawData = fs.readFileSync('config.json');
 const config = JSON.parse(configRawData);
 
-function getNode(nameProject) {
-    const projects = {
-        'celestia': new CelestiaNode(
-            config.security.STAKEME_MONIKER,
-            config.security.STAKEME_WALLET,
-            config.security.STAKEME_PASSWORD,
-        ),
-        'nibiru': new NibiruNode(
-            config.security.STAKEME_MONIKER,
-            config.security.STAKEME_WALLET,
-            config.security.STAKEME_PASSWORD,
-        ),
-        'gitopia': new GitopiaNode(
-            config.security.STAKEME_MONIKER,
-            config.security.STAKEME_WALLET,
-            config.security.STAKEME_PASSWORD,
-        )
+function NodeManager() {
+    function getNode(nameProject) {
+        const projects = {
+            'celestia': new CelestiaNode(
+                config.security.STAKEME_MONIKER,
+                config.security.STAKEME_WALLET,
+                config.security.STAKEME_PASSWORD,
+            ),
+            'nibiru': new NibiruNode(
+                config.security.STAKEME_MONIKER,
+                config.security.STAKEME_WALLET,
+                config.security.STAKEME_PASSWORD,
+            ),
+            'gitopia': new GitopiaNode(
+                config.security.STAKEME_MONIKER,
+                config.security.STAKEME_WALLET,
+                config.security.STAKEME_PASSWORD,
+            )
+        }
+        return projects[nameProject];
     }
-    return projects[nameProject];
+    return {
+        getNode
+    }
 }
 
-module.exports = { getNode }
+
+module.exports = NodeManager

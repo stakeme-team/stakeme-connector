d68ff96a49cb49905effd807f49de8f0bcab40b1
stakemepro <admin@stakeme.pro>
2022-12-10 01:12:41 +1000

    fix

--- core/conrollers-api/cosmos/cosmosEndpoint.js
+++ core/conrollers-api/cosmos/cosmosEndpoint.js
@@ -17,130 +17,137 @@ module.exports = (NodeManager) => {
         const query = req.body;
         const type = query.type;
         const project = query.project;
-
-        switch (type) {
-            case 'info':
-                const message = JSON.stringify(NodeManager.getNode(project).info());
-                return res.status(200).json({
-                    type: 'info',
-                    message: message
-                });
-            case 'install':
-                try {
-                    const message = NodeManager.getNode(project).install();
+        try {
+            switch (type) {
+                case 'info':
+                    const message = JSON.stringify(NodeManager.getNode(project).info());
                     return res.status(200).json({
-                        type: 'install',
+                        type: 'info',
                         message: message
                     });
-                } catch (e) {
-                    return res.status(400).json({
-                        type: 'install',
-                        message: 'Error install node'
-                    });
-                }
-            case 'restart':
-                try {
-                    const message = NodeManager.getNode(project).restart();
+                case 'install':
+                    try {
+                        const message = NodeManager.getNode(project).install();
+                        return res.status(200).json({
+                            type: 'install',
+                            message: message
+                        });
+                    } catch (e) {
+                        return res.status(400).json({
+                            type: 'install',
+                            message: 'Error install node'
+                        });
+                    }
+                case 'restart':
+                    try {
+                        const message = NodeManager.getNode(project).restart();
+                        return res.status(200).json({
+                            type: 'restart',
+                            message: message
+                        });
+                    } catch (e) {
+                        return res.status(400).json({
+                            type: 'restart',
+                            message: 'Error restart node'
+                        });
+                    }
+                case 'logs':
                     return res.status(200).json({
-                        type: 'restart',
-                        message: message
+                        type: 'logs',
+                        message: NodeManager.getNode(project).logs()
                     });
-                } catch (e) {
-                    return res.status(400).json({
-                        type: 'restart',
-                        message: 'Error restart node'
-                    });
-                }
-            case 'logs':
-                return res.status(200).json({
-                    type: 'logs',
-                    message: NodeManager.getNode(project).logs()
-                });
-            case 'stop':
-                try {
-                    const message = NodeManager.getNode(project).stop();
+                case 'stop':
+                    try {
+                        const message = NodeManager.getNode(project).stop();
+                        return res.status(200).json({
+                            type: 'stop',
+                            message: message
+                        });
+                    } catch (e) {
+                        return res.status(400).json({
+                            type: 'stop',
+                            message: 'Error stop node'
+                        });
+                    }
+                case 'delete':
+                    const messageDelete = NodeManager.getNode(project).delete();
                     return res.status(200).json({
-                        type: 'stop',
-                        message: message
+                        type: 'delete',
+                        message: messageDelete
                     });
-                } catch (e) {
-                    return res.status(400).json({
-                        type: 'stop',
-                        message: 'Error stop node'
+                case 'exist':
+                    return res.status(200).json({
+                        type: 'exist',
+                        message: NodeManager.getNode(project).exist()
                     });
-                }
-            case 'delete':
-                const messageDelete = NodeManager.getNode(project).delete();
-                return res.status(200).json({
-                    type: 'delete',
-                    message: messageDelete
-                });
-            case 'exist':
-                return res.status(200).json({
-                    type: 'exist',
-                    message: NodeManager.getNode(project).exist()
-                });
-            case 'wallet':
-                const argument = query.argument;
-                switch (argument) {
-                    case 'exist':
-                        return res.status(200).json({
-                            type: 'wallet',
-                            argument: argument,
-                            message: NodeManager.getNode(project).existWallet()
-                        });
-                    case 'create':
-                        return res.status(200).json({
-                            type: 'wallet',
-                            argument: argument,
-                            message: NodeManager.getNode(project).createWallet()
-                        });
-                    case 'existValidator':
-                        return res.status(200).json({
-                            type: 'wallet',
-                            argument: argument,
-                            message: NodeManager.getNode(project).existValidator()
-                        });
-                    case 'createValidator':
-                        const moniker = query.moniker;
-                        const details = query.details;
-                        const identify = query.identify;
-                        return res.status(200).json({
-                            type: 'wallet',
-                            argument: argument,
-                            message: NodeManager.getNode(project).createValidator(moniker, details, identify)
-                        });
-                    case 'sendTokens':
-                        const toWallet = query.toWallet;
-                        return res.status(200).json({
-                            type: 'wallet',
-                            argument: argument,
-                            message: NodeManager.getNode(project).sendTokens(toWallet, query.amount)
-                        });
-                    case 'delegateTokens':
-                        const toValoper = query.toValoper;
-                        return res.status(200).json({
-                            type: 'wallet',
-                            argument: argument,
-                            message: NodeManager.getNode(project).delegateTokens(toValoper, query.amount)
-                        });
-                    case 'faucet':
-                        return res.status(200).json({
-                            type: 'wallet',
-                            argument: argument,
-                            message: NodeManager.getNode(project).faucet()
-                        });
-                }
-                break;
-            case 'setUser':
-                const telegramUserId = query.telegramUserId
-                config['telegramUserId'] = telegramUserId;
-                const data = JSON.stringify(config, null, 2);
-                fs.writeFileSync('config.json', data);
-                return res.status(200).json({
-                    type: 'setUser',
-                    message: 'success'
-                });
+                case 'wallet':
+                    const argument = query.argument;
+                    switch (argument) {
+                        case 'exist':
+                            return res.status(200).json({
+                                type: 'wallet',
+                                argument: argument,
+                                message: NodeManager.getNode(project).existWallet()
+                            });
+                        case 'create':
+                            return res.status(200).json({
+                                type: 'wallet',
+                                argument: argument,
+                                message: NodeManager.getNode(project).createWallet()
+                            });
+                        case 'existValidator':
+                            return res.status(200).json({
+                                type: 'wallet',
+                                argument: argument,
+                                message: NodeManager.getNode(project).existValidator()
+                            });
+                        case 'createValidator':
+                            const moniker = query.moniker;
+                            const details = query.details;
+                            const identify = query.identify;
+                            return res.status(200).json({
+                                type: 'wallet',
+                                argument: argument,
+                                message: NodeManager.getNode(project).createValidator(moniker, details, identify)
+                            });
+                        case 'sendTokens':
+                            const toWallet = query.toWallet;
+                            return res.status(200).json({
+                                type: 'wallet',
+                                argument: argument,
+                                message: NodeManager.getNode(project).sendTokens(toWallet, query.amount)
+                            });
+                        case 'delegateTokens':
+                            const toValoper = query.toValoper;
+                            return res.status(200).json({
+                                type: 'wallet',
+                                argument: argument,
+                                message: NodeManager.getNode(project).delegateTokens(toValoper, query.amount)
+                            });
+                        case 'faucet':
+                            return res.status(200).json({
+                                type: 'wallet',
+                                argument: argument,
+                                message: NodeManager.getNode(project).faucet()
+                            });
+                    }
+                    break;
+                case 'setUser':
+                    const telegramUserId = query.telegramUserId
+                    config['telegramUserId'] = telegramUserId;
+                    const data = JSON.stringify(config, null, 2);
+                    fs.writeFileSync('config.json', data);
+                    return res.status(200).json({
+                        type: 'setUser',
+                        message: 'success'
+                    });
+            }
+        } catch (e) {
+            console.log(e);
+            return res.status(400).json({
+                message: "stakeme-connector error. Check logs"
+            });
         }
+
     };
 }

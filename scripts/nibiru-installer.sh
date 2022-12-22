576c0a0d6661dccf94599a8ba634ee41cfc4b7e8
stakemepro <admin@stakeme.pro>
2022-12-23 03:46:05 +1000

    update to nibiru-testnet-2

--- scripts/nibiru-installer.sh
+++ scripts/nibiru-installer.sh
@@ -6,7 +6,7 @@ function printLog {
   echo -e "${BLUE}${1}${NC}"
 }
 
-CHAIN_ID="nibiru-testnet-1"
+CHAIN_ID="nibiru-testnet-2"
 TOKEN="nibid"
 
 printLog "Update packages"
@@ -19,7 +19,7 @@ cd || return
 rm -rf nibiru
 git clone https://github.com/NibiruChain/nibiru
 cd nibiru || return
-git checkout v0.15.0
+git checkout v0.16.2
 make install
 $TOKEN version
 
@@ -29,15 +29,15 @@ $TOKEN config chain-id $CHAIN_ID
 $TOKEN init $STAKEME_MONIKER --chain-id $CHAIN_ID
 
 printLog "Download genesis and setup config"
-curl -s https://rpc.testnet-1.nibiru.fi/genesis | jq -r .result.genesis > $HOME/.nibid/config/genesis.json
-sha256sum $HOME/.nibid/config/genesis.json # b58b61beb34f0d9e45ec2f1449f6600acef428b401976dc90edb9d586a412ed2
+curl -s https://rpc.testnet-2.nibiru.fi/genesis | jq -r .result.genesis > $HOME/.nibid/config/genesis.json
+sha256sum $HOME/.nibid/config/genesis.json # 5cedb9237c6d807a89468268071647649e90b40ac8cd6d1ded8a72323144880d
 
 ADDRBOOK_NAME=$(curl -s http://nibiru.stakeme.pro:8080/public/ | egrep -o ">nibiru_addrbook.*\.json" | tr -d ">")
 curl -s http://nibiru.stakeme.pro:8080/public/$ADDRBOOK_NAME > $HOME/.nibid/config/addrbook.json
 
 sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001unibi"|g' $HOME/.nibid/config/app.toml
 seeds="ae357e14309640ca33cde597b37f0a91e63a32bd@nibiru.stakeme.pro:36656"
-peers="ae357e14309640ca33cde597b37f0a91e63a32bd@nibiru.stakeme.pro:36656,b32bb87364a52df3efcbe9eacc178c96b35c823a@nibiru-testnet.nodejumper.io:26656,968472e8769e0470fadad79febe51637dd208445@65.108.6.45:60656,ff597c3eea5fe832825586cce4ed00cb7798d4b5@rpc.nibiru.ppnv.space:10656,37713248f21c37a2f022fbbb7228f02862224190@35.243.130.198:26656,ff59bff2d8b8fb6114191af7063e92a9dd637bd9@35.185.114.96:26656,cb431d789fe4c3f94873b0769cb4fce5143daf97@35.227.113.63:26656"
+peers="ae357e14309640ca33cde597b37f0a91e63a32bd@nibiru.stakeme.pro:36656"
 sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/.nibid/config/config.toml
 
 printLog "Setup pruning and other settings"
